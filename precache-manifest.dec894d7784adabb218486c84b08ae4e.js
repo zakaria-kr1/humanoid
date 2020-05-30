ae3cbc1cfb6bb5eba4d3cf7c9399241ecaa4f5aa
zakaria <zakaria.karwan@gmail.com>
2020-05-30 13:18:51 +0000

    Updates

---
+++ precache-manifest.dec894d7784adabb218486c84b08ae4e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bbfb376d4e277a2f54e165f93f0fe46",
+    "url": "/humanoid/index.html"
+  },
+  {
+    "revision": "a94ad84ed08f0a31e7b4",
+    "url": "/humanoid/static/css/main.a142fb8a.chunk.css"
+  },
+  {
+    "revision": "f6d2ab47bb337dc705a8",
+    "url": "/humanoid/static/js/2.b67fbdca.chunk.js"
+  },
+  {
+    "revision": "a94ad84ed08f0a31e7b4",
+    "url": "/humanoid/static/js/main.921a264d.chunk.js"
+  },
+  {
+    "revision": "0175d1f6df3c2b9a91f2",
+    "url": "/humanoid/static/js/runtime~main.1735c3cb.js"
+  }
+]);
